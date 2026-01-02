c7435405347616b006cb16c02eb7cde710177b01
ZenoCoding <tychoyoung@gmail.com>
2026-01-02 11:34:36 -0800

    Initial Commit

---
+++ Minute/MinuteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinuteApp.swift
+//  Minute
+//
+//  Created by Tycho Young on 1/2/26.
+//
+
+import SwiftUI
+
+@main
+struct MinuteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
